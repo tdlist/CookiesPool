19aee824e62ee2b8d78038a15b7b8c994a06444b
许智海 <18829040039@163.com>
2019-09-17 20:05:15 +0800

    cookie 池服务

---
+++ api.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/16 21:04
+# @Author  : Esbiya
+# @Email   : 18829040039@163.com
+# @File    : api.py
+# @Software: PyCharm
+
+import json
+from flask import Flask, g
+from db import *
+from config import *
+
+
+__all__ = ['app']
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h2>Welcome to Cookie Pool System</h2>'
+
+
+def get_conn():
+    """
+    初始化数据库
+    :return:
+    """
+    for website in GENERATOR_MAP:
+        if not hasattr(g, website):
+            setattr(g, website + '_cookies', eval('RedisClient' + '("cookies", "' + website + '")'))
+            setattr(g, website + '_accounts', eval('RedisClient' + '("accounts", "' + website + '")'))
+    return g
+
+
+@app.route('/<website>/random')
+def random(website):
+    """
+    获取随机 Cookie
+    :param website:
+    :return:
+    """
+    g = get_conn()
+    cookies = getattr(g, website + '_cookies').random()
+    return cookies
+
+
+@app.route('/<website>/add/<username>/<password>')
+def add(website, username, password):
+    """
+    添加用户
+    :param website: 站点
+    :param username: 用户名
+    :param password: 密码
+    :return:
+    """
+    g = get_conn()
+    print(username, password)
+    getattr(g, website + '_accounts').set(username, password)
+    return json.dumps({'status': '1'})
+
+
+@app.route('/<website>/count')
+def count(website):
+    """
+    获取 cookie 总数
+    :param website:
+    :return:
+    """
+    g = get_conn()
+    count = getattr(g, website + '_cookies').count()
+    return json.dumps({'status': '1', 'count': count})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8778)
